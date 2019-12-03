fbe2d96a3c47624166ce0ad1130d3140d766ced5
sethripman <sethripman@gmail.com>
2019-12-03 14:05:33 -0800

    finished scaffolding, imported whiteboarding image to assets, wrote multiple solutions

---
+++ challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,46 @@
+// Reverse half the array in place
+export const halfReverse = (array) => {
+  for(let i = 0; i <= Math.floor((array.length - 1) / 2); i++) {
+    let current = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = current;
+  }
+  return array;
+};
+
+//Bogosort implementation of reverse - array.reverse used for confirming successful sort but not for reversing operation on array
+const randomize = (array) => {
+  let newArray = array.slice();
+  for(let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
+
+const successfulReverse = (array, newArray) => {
+  if(newArray === array.reverse()) return true;
+  return false;
+};
+
+export const dankReverse = (array) => {
+  let finished = false;
+  let newArray = [];
+  while(finished === false){
+    newArray = randomize(array);
+    finished = successfulReverse(array, newArray);
+  }
+  return newArray;
+};
+
+// Pop reverse, two arrays
+export const popReverse = (array) => {
+  let newArray = [];
+  for(let i = array.length; i > 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+//map reverse, functional programming implementation
+export const mapReverse = array => [...array].map(array.pop, array);
