08d1d4c8cfae989c0b41718df3e68bc666cf352c
sethripman <sethripman@gmail.com>
2019-12-12 16:30:29 -0800

    merge function basics written

--- challenges/ll-merge/ll-merge.js
+++ challenges/ll-merge/ll-merge.js
@@ -1,8 +1,18 @@
 const mergeLists = (linkedListOne, linkedListTwo) => {
 
-    if(!linkedListOne.head) return linkedListTwo;
+  if(!linkedListOne.head) return linkedListTwo;
 
-    
+  let currentOne, currentTwo, temp;
+  currentOne = linkedListOne.head;
+  currentTwo = linkedListTwo.head;
+
+  while(currentTwo) {
+    temp = currentOne.next;
+    currentOne.next = currentTwo;
+    currentTwo.next = temp;
+
+    currentOne = currentOne.next;
+  }
     
   return linkedListOne;
 };
