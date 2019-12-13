a9d90f85a4ba455729c0955cd42c5ce7ed1e0e7a
sethripman <sethripman@gmail.com>
2019-12-12 16:48:56 -0800

    passing tests for empty lists

--- __tests__/ll-merge.test.js
+++ __tests__/ll-merge.test.js
@@ -47,4 +47,23 @@ describe('linked list merge function', () => {
 
     expect(mergedList.toString()).toEqual('1 -> 5 -> 3 -> 9 -> 2');
   });
+
+  it('should handle second list being empty', () => {
+    const linkedList1 = new LinkedList();
+    linkedList1.insert(2);
+    linkedList1.insert(3);
+    linkedList1.insert(1);
+    const linkedList2 = new LinkedList();
+    const mergedList = mergeLists(linkedList1, linkedList2);
+    expect(mergedList.toString()).toEqual('1 -> 3 -> 2');
+  });
+  it('should handle first list being empty', () => {
+    const linkedList1 = new LinkedList();
+    const linkedList2 = new LinkedList();
+    linkedList2.insert(2);
+    linkedList2.insert(3);
+    linkedList2.insert(1);
+    const mergedList = mergeLists(linkedList1, linkedList2);
+    expect(mergedList.toString()).toEqual('1 -> 3 -> 2');
+  });
 });
