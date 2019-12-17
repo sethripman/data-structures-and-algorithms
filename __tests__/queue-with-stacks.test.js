cecb119afc8db3357da34a6486a7e75e272ea947
sethripman <sethripman@gmail.com>
2019-12-16 17:03:27 -0800

    working on constructor test

--- __tests__/queue-with-stacks.test.js
+++ __tests__/queue-with-stacks.test.js
@@ -1,8 +1,9 @@
 const { PseudoQueue } = require('../challenges/queueWithStacks/queue-with-stacks');
 
-describe('queue with stacks data structure', () => {
-  it('should take an array and a value as input and return a new array identical to the original except with the value inserted in the middle position of the array', () => {
-    expect(arrayShift([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
-    expect(arrayShift([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
+describe('queue with two stacks data structure', () => {
+  it('should have a constructor function that creates a Pseudoqueue instance with first stack and second stack properties', () => {
+    const testQueue = new PseudoQueue();
+    expect(testQueue).toHaveProperty(firstStack);
+    expect(testQueue).toHaveProperty(secondStack);
   });
 });
--- challenges/queueWithStacks/queue-with-stacks.js
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -1,91 +1,50 @@
-class Stack { // from demo code
-  constructor(val, next = null) {
-    this.value = val;
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
     this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    const node = new Node(value, this.top);
+    this.top = node;
+  }
+
+  pop() {
+    if(!this.top) return null;
+
+    const top = this.top;
+    this.top = top.next;
+    return top;
+  }
+
+  peek() {
+    if(!this.top) return null;
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return !this.top;
   }
 }
   
 class PseudoQueue {
   constructor() {
-    this.head = null;
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
   }
   
-  insert(val) {
-    const node = new Node(val, this.head);
-    this.head = node;
-  }
-  
-  includes(val) {
-    let searchNode = this.head;
-    while(searchNode) {
-      if(val === searchNode.value) return true;
-      searchNode = searchNode.next;
-    }
-  
-    return false;
-  }
-  
-  toString() {
-    if(!this.head) return '';
-  
-    const nodes = [];
-    let node = this.head;
-    while(node) {
-      nodes.push(node.value);
-      node = node.next;
-    }
-  
-    return nodes.join(' -> ');
-  }
-
-  append(value) {
-    const newNode = new Node(value);
-    if(!this.head) {
-      this.head = newNode;
-    } else {
-      let searchNode = this.head;
-      while(searchNode) {
-        searchNode = searchNode.next;
-      }
-      searchNode.next = newNode;
-    }
-  }
-
-  insertBefore(value, newValue) {
-    let currentNode = this.head;
-    const newNode = new Node(newValue);
-    while(currentNode.next.value !== value){
-      currentNode = currentNode.next;
-    }
+  enqueue(value) {
     
-    newNode.next = currentNode.next;
-    currentNode.next = newNode;
   }
 
-  insertAfter(value, newValue){
-    const newNode = new Node(newValue);
-    let currentNode = this.head;
-    while(currentNode.value !== value){
-      currentNode = currentNode.next;
-    }
+  dequeue() {
 
-    newNode.next = currentNode.next;
-    currentNode.next = newNode;
-  }
-
-  kthFromEnd(k){
-    const arrayFromList = [];
-    let current = this.head;
-
-    if(Math.sign(k) === -1 || !current) return 'Exception';
-
-    while(current) {
-      arrayFromList.push(current.value);
-      current = current.next;
-    }
-
-    if(k > arrayFromList.length) return 'Exception';
-    return mapReverse(arrayFromList)[k];
   }
 }
   
