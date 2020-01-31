bbbcefbb247c42fad35feefc2a51c8ce73b043b5
sethripman <sethripman@gmail.com>
2020-01-30 17:26:26 -0800

    writing tests

--- __tests__/Tree.test.js
+++ __tests__/Tree.test.js
@@ -1,26 +1,30 @@
-const { PseudoQueue } = require('../challenges/queueWithStacks/queue-with-stacks');
+const { BinarySearchTree } = require('../challenges/tree/Tree');
 
-describe('queue with two stacks data structure', () => {
-  it('should have a constructor function that creates a Pseudoqueue instance with first stack and second stack properties', () => {
-    const testQueue = new PseudoQueue();
-    expect(testQueue.firstStack).toEqual({
+describe('binary search tree recursive implementation', () => {
+  it('should have a constructor function that creates a BST with a null root if no arguments are passed', () => {
+    const testTree = new BinarySearchTree();
+    expect(testTree.root).toEqual(null);
+  });
+  it('should have a constructor function that creates a BST with a root node if a value is passed', () => {
+    const testTree = new BinarySearchTree();
+    expect(testTree.firstStack).toEqual({
       top: null
     });
-    expect(testQueue.secondStack).toEqual({
+    expect(testTree.secondStack).toEqual({
       top: null
     });
   });
 
   it('should have an enqueue function that takes a value and pushes it onto the firstStack', () => {
-    const testQueue = new PseudoQueue();
-    testQueue.enqueue(1);
-    expect(testQueue.firstStack.peek()).toBe(1);
+    const testTree = new BinarySearchTree();
+    testTree.enqueue(1);
+    expect(testTree.firstStack.peek()).toBe(1);
   });
 
   it('should have an dequeue function that returns the value from the node at the front of the queue using FIFO ordering', () => {
-    const testQueue = new PseudoQueue();
-    testQueue.enqueue(1);
-    testQueue.enqueue(2);
-    expect(testQueue.dequeue()).toBe(1);
+    const testTree = new BinarySearchTree();
+    testTree.enqueue(1);
+    testTree.enqueue(2);
+    expect(testTree.dequeue()).toBe(1);
   });
 });
