e61cb72efb4d4e87f088cbfd90abd2b4da086de6
sethripman <sethripman@gmail.com>
2020-01-31 17:02:52 -0800

    got help from Ryan on approach for the Code challenge

--- challenges/fizzbuzz-tree/FizzbuzzTree.js
+++ challenges/fizzbuzz-tree/FizzbuzzTree.js
@@ -1,3 +1,5 @@
+// define a new tree where traversal returns an array of nodes instead of the values therein, then mutate the nodes, and the original BST will be correct without making a new BST
+
 const { BinarySearchTree } = require('../tree/Tree');
 
 const FizzbuzzTree = ( BST ) => {
@@ -5,5 +7,5 @@ const FizzbuzzTree = ( BST ) => {
 
   const mutateArray = BST.
 
-  return 
+  return
 };
